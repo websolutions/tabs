13054dc04ea5706cfc30376a5eb572ffd50bbf7f
Maxwell Barvian <max@barvian.me>
2014-07-18 15:50:38 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-tabs
-====
+# Tabs
+
+Content tabs that toggle content when clicked.
+
+## Usage
+
+Example DOM structure:
+```html
+<div class="tabs clearfix">
+  <ul class="tab-navigation clearfix wide">
+    <li class="selected">
+      <a href="#tab1">Tab Title 1</a>
+    </li>
+    <li>
+      <a href="#tab2">Tab Title 2</a>
+    </li>
+  </ul>
+  <div class="tab-container">
+    <div id="tab1" class="clear">
+      <h3 class="narrow">Tab Title 1</h3>
+      <p>Tab Content 1</p>
+    </div>
+    <div id="tab2" class="clear narrow">
+      <h3 class="narrow">Tab Title 2</h3>
+      <p>Tab Content 2</p>
+    </div>
+  </div>
+</div>
+```
+
+Initialize with jQuery:
+```javascript
+$(".tabs").tabs()
+```
---
+++ js/wsol.tabs.js
@@ -0,0 +1,37 @@
+﻿(function ($) {
+  $.fn.tabs = function (options) {
+
+    var defaults = {
+      SelectedClass: "selected",
+      NavigationLinkSelector: "> ul > li > a",
+      ContainerSelector: "> div.tab-container > div",
+      HiddenClass: "narrow"
+    };
+
+    var settings = $.extend({}, defaults, options);
+
+    $(this).each(function () {
+      var $TabPanel = $(this);
+      var $NavigationLinks = $(settings.NavigationLinkSelector, $TabPanel);
+      var $Containers = $(settings.ContainerSelector, $TabPanel);
+
+      $Containers.not(':eq(0)').addClass(settings.HiddenClass);
+      $NavigationLinks.first().parent().addClass(settings.SelectedClass);
+
+      $NavigationLinks.each(function (index, value) {
+        $(this).bind("click", function (e) {
+          e.preventDefault();
+
+          var $this = $(this);
+
+          $NavigationLinks.parent().removeClass(settings.SelectedClass);
+          $Containers.addClass(settings.HiddenClass);
+          $Containers.parent().find($this.attr("href")).removeClass(settings.HiddenClass);
+
+          $this.parent().addClass(settings.SelectedClass);
+        });
+      });
+    });
+    return this;
+  }
+})(jQuery);
