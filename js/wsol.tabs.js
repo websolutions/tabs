c66926cdf7e00add6e8b44ac4722c8732c1e97da
Nick Melville <nmelville@wsol.com>
2022-05-17 14:45:40 -0500

    adding accessibility features

--- js/wsol.tabs.js
+++ js/wsol.tabs.js
@@ -1,5 +1,5 @@
 ﻿/**
- * wsol.tabs.js 3.1.3
+ * wsol.tabs.js 4.0.0
  * http://github.com/websolutions/tabs
  */
 
@@ -27,7 +27,23 @@
       base.options.firstRun = true;
 
       base.$navLinks = base.$el.find(base.options.navigationLinkSelector).filter('[href^="#"]');
+      base.$navButtons = $();
       base.$containers = base.$el.find(base.options.containerSelector);
+
+      base.$navLinks.first().closest('ul, div').attr('role', 'tablist');
+      base.$navLinks.each(function() {
+        var controls = $(this).attr('href').slice(1),
+          newId = controls + "-button",
+          $newButton = $('<button role="tab" aria-controls="'+controls+'" id="'+newId+'" ><span class="expandable-header-wrap">'+$(this).text()+'</span></button>')
+
+        $(this).after($newButton);
+        $(this).hide();
+        base.$containers.filter('#'+controls).attr('aria-labelledby', newId);
+        base.$navButtons = base.$navButtons.add($newButton);
+      })
+      base.$containers.attr('hidden', 'hidden').attr('tabindex', '0').attr('role', 'tabpanel');
+      
+      base.$navLinks = base.$navButtons;
 
       base.changeTab(0);
 
@@ -36,18 +52,18 @@
     };
 
     base.changeTab = function(tab) {
-      base.$navLinks.parent().removeClass(base.options.selectedClass);
-      base.$containers.addClass(base.options.hiddenClass);
+      base.$navLinks.attr('aria-selected', 'false').parent().removeClass(base.options.selectedClass);
+      base.$containers.addClass(base.options.hiddenClass).attr('hidden', 'hidden');
 
       if (typeof tab === "number") {
-        tab = base.$navLinks.eq(tab).attr("href");
+        tab = "#" + base.$navLinks.eq(tab).attr("aria-controls");
       }
 
-      base.$containers.filter(tab).removeClass(base.options.hiddenClass).toggleClass(base.options.accordionHiddenClass);
-      base.$navLinks.filter('[href="' + tab + '"]').parent().addClass(base.options.selectedClass).toggleClass(base.options.accordionHiddenClass);
+      base.$containers.filter(tab).removeClass(base.options.hiddenClass).toggleClass(base.options.accordionHiddenClass).removeAttr('hidden');
+      base.$navLinks.filter('[aria-controls="' + tab.replace("#", "") + '"]').attr('aria-selected', 'true').parent().addClass(base.options.selectedClass).toggleClass(base.options.accordionHiddenClass);
 
       base.$containers.filter(function () {return $(this).attr('id') != tab.replace("#", "") }).addClass(base.options.accordionHiddenClass);
-      base.$navLinks.filter(function () { return $(this).attr('href') != tab }).parent().addClass(base.options.accordionHiddenClass);
+      base.$navLinks.filter(function () { return $(this).attr('aria-controls') != tab.replace("#", "") }).parent().addClass(base.options.accordionHiddenClass);
 
       if (base.options.autoScroll && !base.options.firstRun) {
         var $elm = base.$containers.filter(tab)
@@ -67,17 +83,20 @@
 
     base._tabHandler = function(event) {
       var $target = $(this);
-      $target.is("a") && event.preventDefault(); // if target is a link, prevent default action
+      event.preventDefault(); // if target is a link, prevent default action
 
-      base.changeTab($target.attr("href"));
+      base.changeTab("#" + $target.attr("aria-controls"));
     };
 
     base.destroy = function() {
-      base.$navLinks.parent().removeClass(base.options.selectedClass);
-      base.$containers.removeClass(base.options.hiddenClass);
+      base.$navLinks.parent().removeClass(base.options.selectedClass).removeClass(base.options.accordionHiddenClass);
+      base.$navLinks.first().closest('ul, div').removeAttr('role');
+      base.$el.find(base.options.navigationLinkSelector).filter('[href^="#"]').show();
+      base.$containers.removeClass(base.options.hiddenClass).removeClass(base.options.accordionHiddenClass).removeAttr('hidden').removeAttr('tabindex').removeAttr('role').removeAttr('aria-labelledby');
 
       // Remove event handlers
       base.$navLinks.off(".wsol.tabs");
+      base.$navLinks.remove();
     };
 
     base.init();
